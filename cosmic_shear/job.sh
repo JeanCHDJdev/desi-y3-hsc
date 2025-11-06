7c65c7a85faccc77ad92c51810b4082edb10430a
JeanCHDJdev <jean.choppindejanvrydev@gmail.com>
2025-11-06 07:26:23 -0800

    Added config files for S8 and nersc job, update to notebooks for S8

--- cosmic_shear/job.sh
+++ cosmic_shear/job.sh
@@ -2,17 +2,17 @@
 
 #SBATCH --qos regular
 #SBATCH --nodes 4
-#SBATCH --time 10:00:00
+#SBATCH --time 11:00:00
 #SBATCH --constraint cpu
 #SBATCH --account m3058
 #SBATCH --job-name real
-#SBATCH --output /global/cfs/projectdirs/desi/users/jchdj/desi-y3-hsc/cosmic-shear/logs/cshear-real-%j.out
-#SBATCH --error /global/cfs/projectdirs/desi/users/jchdj/desi-y3-hsc/cosmic-shear/logs/cshear-real-%j.err
+#SBATCH --output /global/cfs/projectdirs/desi/users/jchdj/desi-y3-hsc/cosmic_shear/logs/cshear-real-%j.out
+#SBATCH --error /global/cfs/projectdirs/desi/users/jchdj/desi-y3-hsc/cosmic_shear/logs/cshear-real-%j.err
 
 umask 0002
 export TQDM_MININTERVAL=120 # export to two minutes
 
 source /dvs_ro/cfs/projectdirs/des/zuntz/cosmosis-global/setup-cosmosis3
 cd /global/cfs/projectdirs/desi/users/jchdj/software/cosmosis-standard-library/
-command="cosmosis --mpi /global/cfs/projectdirs/desi/users/jchdj/software/cosmosis-standard-library/examples/hsc-y3-shear-real.ini"
+command="cosmosis --mpi /global/cfs/projectdirs/desi/users/jchdj/desi-y3-hsc/cosmic_shear/config/real/hsc-y3-shear-real.ini"
 srun -n 512 $command
