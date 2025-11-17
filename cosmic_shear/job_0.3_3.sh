3b338884915b59e6075b249b6e4473edc7906caf
JeanCHDJdev <jean.choppindejanvrydev@gmail.com>
2025-11-17 02:21:44 -0800

    Added various cosmic shear jobs for NERSC

---
+++ cosmic_shear/job_0.3_3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+
+#SBATCH --qos regular
+#SBATCH --nodes 4
+#SBATCH --time 10:00:00
+#SBATCH --constraint cpu
+#SBATCH --account m3058
+#SBATCH --job-name real-0.3-3-fiducial
+#SBATCH --output /global/cfs/projectdirs/desi/users/jchdj/desi-y3-hsc/cosmic_shear/logs/cshear-real-0.3-3-fiducial-%j.out
+#SBATCH --error /global/cfs/projectdirs/desi/users/jchdj/desi-y3-hsc/cosmic_shear/logs/cshear-real-0.3-3-fiducial-%j.err
+
+umask 0002
+export TQDM_MININTERVAL=240 # export to four minutes
+
+source /dvs_ro/cfs/projectdirs/des/zuntz/cosmosis-global/setup-cosmosis3
+cd /global/cfs/projectdirs/desi/users/jchdj/software/cosmosis-standard-library/
+command="cosmosis --mpi /global/cfs/projectdirs/desi/users/jchdj/desi-y3-hsc/cosmic_shear/config/real-.3-3/fiducial/hsc-y3-shear-real.ini"
+srun -n 512 $command
