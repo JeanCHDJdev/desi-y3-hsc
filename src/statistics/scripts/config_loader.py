5da6a57e06064ae82df2c4c7b70a548627049b33
Quentin Lavier <qlavier@login17.chn.perlmutter.nersc.gov>
2026-04-27 05:52:41 -0700

    modif pipeline correlations

---
+++ src/statistics/scripts/config_loader.py
@@ -0,0 +1,9 @@
+import numpy as np
+username = 'qlavier'
+bins_bgs = np.arange(0.0, 0.65, 0.05)
+bins_lrg = np.arange(0.3, 1.25, 0.05)
+bins_elg = np.arange(0.7, 1.65, 0.05)
+bins_qso = np.arange(0.7, 2.85, 0.05)
+bins_hsc = np.array([0.3, 0.6, 0.9, 1.2, 1.5, 2.0, 2.5, 6.0])
+hsc_catalog = 'hscpdr3_i25_imagerr'
+desi_version = 'DR2'
---
+++ src/statistics/scripts/test.py
@@ -0,0 +1,2 @@
+for key in [1,2,3]:
+    print(2)
